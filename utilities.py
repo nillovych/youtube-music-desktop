9d620fb4ed44798c7dddffeff1f99967f72d734b
Danylo <danykyurkevych@gmail.com>
2024-03-08 10:22:00 +0100

    Initialisation of the blues

---
+++ utilities.py
@@ -0,0 +1,12 @@
+import requests
+from PIL import Image
+
+
+def download_image(url, save_path):
+    response = requests.get(url)
+    with open(save_path, 'wb') as file:
+        file.write(response.content)
+
+    img = Image.open(save_path)
+    img = crop((105, 45, 370, 320))
+    img.save(save_path)
