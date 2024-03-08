c36010583f79b23a30592ef14f78df9ad0f28914
Danylo <danykyurkevych@gmail.com>
2024-03-08 10:22:00 +0100

    Initialisation of the blues

---
+++ google_service.py
@@ -0,0 +1,8 @@
+from Google import Create_Service
+
+CLIENT_SECRET_FILE = 'client-secret.json'
+API_NAME = 'youtube'
+API_VERSION = 'v3'
+SCOPES = ['https://www.googleapis.com/auth/youtube']
+
+service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
